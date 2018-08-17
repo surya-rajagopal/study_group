ebbc0d47a809f567beb20d232b2bb1568109d933
arvindmadhavan <arvind.madhavan.bits@gmail.com>
2018-08-16 20:34:06 -0700

    Update TrappingRainWater.java

--- arvind/questions-hard/TrappingRainWater.java
+++ arvind/questions-hard/TrappingRainWater.java
@@ -26,14 +26,14 @@ public class TrappingRainWater {
 			ans[0] = height[0];
 			for(int i=1;i<height.length;i++){
 			   ans[i]= Math.max(ans[i-1], height[i]);
-			}
+			} //0(n)
 			
 	    	int res =0;
 	    	int rightmax = height[height.length - 1];
 			for(int i=height.length-2;i>=0;i--){
 				rightmax = Math.max(rightmax, height[i]) ;
 				res+= Math.min(ans[i], rightmax) - height[i];
-			}
+			}//0(n)
 			return res; 
     }
 }
